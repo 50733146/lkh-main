bc0487315a598f8599fd8230e78fc6794cca5de7
lkh-1 <50733146@gm.nfu.edu.tw>
2021-07-01 12:11:54 +0800

    update blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'cmstemplate user'
-SITENAME = 'cmstemplate blog'
+AUTHOR = 'LKH'
+SITENAME = 'LKH blog'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
